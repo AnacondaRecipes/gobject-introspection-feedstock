85194c86a6c41f8500e199a0cfe91ec9c47db6cc
Kai Tietz <ktietz@anaconda.com>
2023-03-10 07:21:10 +0100

    add g-ir-scanner support for Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,13 +5,6 @@ setlocal EnableDelayedExpansion
 set PKG_CONFIG_PATH="%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig"
 set SEARCH_PATH="%BUILD_PREFIX%\Library\"
 
-findstr /m "C:/ci_310/glib_1642686432177/_h_env/Library/lib/z.lib" "%LIBRARY_LIB%\pkgconfig\gio-2.0.pc"
-if %errorlevel%==0 (
-    :: our current glib gio-2.0.pc has zlib dependency set as an absolute path. 
-    powershell -Command "(gc %LIBRARY_LIB%\pkgconfig\gio-2.0.pc) -replace 'Requires:', 'Requires: zlib,' | Out-File -encoding ASCII %LIBRARY_LIB%\pkgconfig\gio-2.0.pc"
-    powershell -Command "(gc %LIBRARY_LIB%\pkgconfig\gio-2.0.pc) -replace 'C:/ci_310/glib_1642686432177/_h_env/Library/lib/z.lib', '' | Out-File -encoding ASCII %LIBRARY_LIB%\pkgconfig\gio-2.0.pc"
-)
-
 :: meson options
 :: (set pkg_config_path so deps in host env can be found)
 set ^"MESON_OPTIONS=^
@@ -46,6 +39,20 @@ ninja -v -C builddir test
 :: install
 ninja -C builddir install -j %CPU_COUNT%
 if errorlevel 1 exit 1
+
+:: There is no concept of #-bangs on Windows, so we create a wrapper to do
+:: its purpose.
+
+echo %PYTHON% %LIBRARY_BIN%\\g-ir-scanner %%* >>%LIBRARY_BIN%\g-ir-scanner.bat
+
+type %LIBRARY_BIN%\gi-ir-scanner.bat
+
+:: Now we need to modify the .pc files using the .bat file instead of directly
+:: the Python file.
+sed -i.bak -E "s|g_ir_scanner=(.*)|g_ir_scanner=\1.bat|g" %PREFIX%\Library\lib\pkgconfig\gobject-introspection-1.0.pc
+sed -i.bak -E "s|g_ir_scanner=(.*)|g_ir_scanner=\1.bat|g" %PREFIX%\Library\lib\pkgconfig\gobject-introspection-no-export-1.0.pc
+del "%PREFIX%\Library\lib\pkgconfig\gobject-introspection-1.0.pc.bak"
+del "%PREFIX%\Library\lib\pkgconfig\gobject-introspection-no-export-1.0.pc.bak"
 
 :: print pkgconfig file
 type "%PREFIX%\Library\lib\pkgconfig\gobject-introspection-1.0.pc"

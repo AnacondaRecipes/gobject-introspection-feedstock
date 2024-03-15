20fa05964737264f1ee2226664b9f380f311bba0
Charles Bousseau <cbousseau@anaconda.com>
2024-03-15 10:32:50 -0400

    update to 1.78.1

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,13 +5,6 @@ setlocal EnableDelayedExpansion
 set PKG_CONFIG_PATH="%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig"
 set SEARCH_PATH="%BUILD_PREFIX%\Library\"
 
-findstr /m "C:/ci_310/glib_1642686432177/_h_env/Library/lib/z.lib" "%LIBRARY_LIB%\pkgconfig\gio-2.0.pc"
-if %errorlevel%==0 (
-    :: our current glib gio-2.0.pc has zlib dependency set as an absolute path. 
-    powershell -Command "(gc %LIBRARY_LIB%\pkgconfig\gio-2.0.pc) -replace 'Requires:', 'Requires: zlib,' | Out-File -encoding ASCII %LIBRARY_LIB%\pkgconfig\gio-2.0.pc"
-    powershell -Command "(gc %LIBRARY_LIB%\pkgconfig\gio-2.0.pc) -replace 'C:/ci_310/glib_1642686432177/_h_env/Library/lib/z.lib', '' | Out-File -encoding ASCII %LIBRARY_LIB%\pkgconfig\gio-2.0.pc"
-)
-
 :: meson options
 :: (set pkg_config_path so deps in host env can be found)
 set ^"MESON_OPTIONS=^
@@ -40,9 +33,9 @@ if errorlevel 1 exit 1
 ninja -v -C builddir -j %CPU_COUNT%
 if errorlevel 1 exit 1
 
-:: test - some errors, ignore test results for now
+:: test
 ninja -v -C builddir test
-@REM if errorlevel 1 exit 1
+if errorlevel 1 exit 1
 
 :: install
 ninja -C builddir install -j %CPU_COUNT%

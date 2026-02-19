f905d2baba7187a544031d21de5f74ab71e6ba79
Viacheslav Ch. <vchychyrko@anaconda.com>
2026-02-19 18:25:27 +0200

    gobject-introspection-1.86.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
-set -ex
+set -euxo pipefail
 
 if [[ "$target_platform" == osx-* ]] ; then
     # dead_strip_dylibs breaks some tests
@@ -15,24 +15,15 @@ export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
 
 mkdir forgebuild
 cd forgebuild
-meson --buildtype=release --prefix="$PREFIX" --wrap-mode=nofallback --backend=ninja -Dlibdir=lib \
-      -Dcairo=enabled -Dpython="$PYTHON" ..
-ninja -v
+meson ${MESON_ARGS} \
+    --prefix="$PREFIX" \
+    --backend=ninja \
+    -Dlibdir=lib \
+    -Dcairo=enabled \
+    -Dpython="$PYTHON" \
+    ..
 
-if [ ! -f "$SRC_DIR/forgebuild/girepository/libregress-1.0${SHLIB_EXT}" ]; then
-  # libregress library, used in testing only, is not found at test time, failing test gitypelibtest
-  # creating the following symlink circumvent this issue 
-  # similar issues reported here:
-  # See https://github.com/macports/macports-ports/blame/master/gnome/gobject-introspection/Portfile
-  # See https://github.com/NixOS/nixpkgs/commit/52f82285f68116aa2d4559ef9628ebfd90b8f336
-
-  ln -s $SRC_DIR/forgebuild/tests/scanner/libregress-1.0${SHLIB_EXT} $SRC_DIR/forgebuild/girepository/libregress-1.0${SHLIB_EXT}
-  ninja test
-  rm $SRC_DIR/forgebuild/girepository/libregress-1.0${SHLIB_EXT}
-else
-  ninja test
-fi
-
-ninja install
+ninja -v -j${CPU_COUNT}
+ninja install -j${CPU_COUNT}
 
 rm -f $PREFIX/lib/libgirepository-*.a $PREFIX/lib/libgirepository-*.la

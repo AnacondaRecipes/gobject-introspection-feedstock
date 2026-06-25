cb099d005652121a08862e817dad0a48318ae1a9
Eric Lundby <Eric.Lundby@gmail.com>
2026-06-25 15:26:01 -0600

    Fix cache problem

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,6 +11,12 @@ echo none /tmp usertemp binary,posix=0 0 0 >>%BUILD_PREFIX%\Library\etc\fstab
 mkdir forgebuild
 cd forgebuild
 
+@REM g-ir-scanner caches under ~/.cache/g-ir-scanner by default. Keep that out of
+@REM the user profile so build/tests stay hermetic and avoid PermissionError when
+@REM the profile cache is locked or not writable (see cachestore.py).
+set "XDG_CACHE_HOME=%CD%\.cache"
+if not exist "%XDG_CACHE_HOME%" mkdir "%XDG_CACHE_HOME%"
+
 @REM pkg-config setup
 FOR /F "delims=" %%i IN ('cygpath.exe -m "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX_M=%%i"
 set PKG_CONFIG_PATH=%LIBRARY_PREFIX_M%/lib/pkgconfig;%LIBRARY_PREFIX_M%/share/pkgconfig
